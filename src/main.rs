86dbc652b272fe54a949ea0254c9f09c9138ac7d
vi17250 <v.aubinaud@protonmail.com>
2025-06-08 23:14:41 +0200

    feat: 🎸 list directories

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::BufRead;
+use std::io::{self};
+use std::process::Command;
+
+fn main() -> io::Result<()> {
+    let program = "du";
+    let deep = 1;
+    let command = Command::new(program)
+        .arg("-h")
+        .arg(format!("-d {deep}"))
+        .output()?
+        .stdout
+        .lines()
+        .map(|line| line.unwrap().replace("\t", " "))
+        .collect::<Vec<String>>();
+    println!("{:?}", command);
+    Ok(())
 }
