d00cb8e325b502629f885d7e4cf431c0ab9b4917
vi17250 <v.aubinaud@protonmail.com>
2025-06-10 08:51:00 +0200

    feat: 🎸 using '-d' make the software recursive

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,20 @@ use std::io::BufRead;
 use std::io::{self};
 use std::process::Command;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Command recursion
+    #[arg(short, long, default_value_t = 1)]
+    deep: u8,
+}
+
 fn main() -> io::Result<()> {
+    let args = Args::parse();
     let program = "du";
-    let deep = 1;
+    let deep = args.deep;
     let command = Command::new(program)
         .arg("-h")
         .arg(format!("-d {deep}"))
