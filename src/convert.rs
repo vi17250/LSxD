bd5423d9a12fc80e224300ffcfbcfc991ad61165
vi17250 <v.aubinaud@protonmail.com>
2025-06-12 23:48:36 +0200

    refactor: 💡 Colored trait applies colors to &str

--- src/convert.rs
+++ src/convert.rs
@@ -1,31 +1,38 @@
 use colored::{ColoredString, Colorize};
 
-pub fn convert(size: &str) -> ColoredString {
-    match size.to_lowercase() {
-        v if v.contains("k") => size.green(),
-        v if v.contains("m") => size.yellow(),
-        v if v.contains("g") => size.red(),
-        v => v.into(),
+pub trait Colored {
+    fn colored(self) -> ColoredString;
+}
+
+impl Colored for &str {
+    fn colored(self) -> ColoredString {
+        let string = match self.to_lowercase() {
+            v if v.contains("k") => self.green(),
+            v if v.contains("m") => self.yellow(),
+            v if v.contains("g") => self.red(),
+            v => v.into(),
+        };
+        string.bold()
     }
 }
 
 mod test {
 
-    use crate::convert::convert;
+    use crate::convert::Colored;
     use colored::Colorize;
 
     #[test]
     fn it_returns_value_below_1Mb() {
-        assert_eq!(convert("10K"), "10K".green());
+        assert_eq!("10K".colored(), "10K".green().bold());
     }
 
     #[test]
     fn it_returns_value_above_1Mb() {
-        assert_eq!(convert("10M"), "10M".yellow());
+        assert_eq!("10M".colored(), "10M".yellow().bold());
     }
 
     #[test]
     fn it_returns_value_above_1Gb() {
-        assert_eq!(convert("10G"), "10G".red());
+        assert_eq!("10G".colored(), "10G".red().bold());
     }
 }
