3794e9939cf8c5eaee953d67e45fc484401bb1e2
vi17250 <v.aubinaud@protonmail.com>
2025-06-17 16:45:44 +0200

    refactor: 💡 commands are in a separate file

--- src/output_color.rs
+++ src/output_color.rs
@@ -1,11 +1,11 @@
 use colored::{ColoredString, Colorize};
 
 pub trait Colored {
-    fn coloring(self) -> ColoredString;
+    fn coloring(&self) -> ColoredString;
 }
 
-impl Colored for &str {
-    fn coloring(self) -> ColoredString {
+impl Colored for String {
+    fn coloring(&self) -> ColoredString {
         let string = match self.to_lowercase() {
             v if v.contains("k") => self.green(),
             v if v.contains("m") => self.yellow(),
@@ -24,16 +24,16 @@ mod test {
 
     #[test]
     fn it_returns_value_below_1Mb() {
-        assert_eq!("10K".coloring(), "10K".green().bold());
+        assert_eq!("10K".to_string().coloring(), "10K".green().bold());
     }
 
     #[test]
     fn it_returns_value_above_1Mb() {
-        assert_eq!("10M".coloring(), "10M".yellow().bold());
+        assert_eq!("10M".to_string().coloring(), "10M".yellow().bold());
     }
 
     #[test]
     fn it_returns_value_above_1Gb() {
-        assert_eq!("10G".coloring(), "10G".red().bold());
+        assert_eq!("10G".to_string().coloring(), "10G".red().bold());
     }
 }
