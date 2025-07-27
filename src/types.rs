c08cb019aa6ed61fad9b03a8c616a0ff15e45dd1
vi17250 <v.aubinaud@protonmail.com>
2025-07-28 00:05:42 +0200

    feat: 🎸 recursively browse directories

--- src/types.rs
+++ src/types.rs
@@ -1,27 +1,28 @@
-#[derive(Debug, PartialEq)]
-pub enum Entity {
-    Directory,
-    File,
+use std::path::PathBuf;
+
+use crate::get_dirs;
+
+#[derive(Clone, Debug)]
+pub struct Directory {
+    pub path: PathBuf,
+    pub children: Vec<Directory>,
 }
 
-impl Entity {
-    pub fn display(&self) -> String {
-        match &self {
-            Entity::File => String::from("📑"),
-            Entity::Directory => String::from("📁"),
+impl Directory {
+    pub fn get_children(&mut self, deepth: i8) {
+        let mut deepth = deepth;
+        if deepth == 0 {
+            return;
+        }
+        deepth = deepth - 1;
+        for child in &mut self.children {
+            child.children = get_dirs(child.path.clone());
+        }
+
+        if deepth > 0 {
+            for mut p in &mut self.children {
+                p.get_children(deepth);
+            }
         }
     }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct File {
-    pub name: String,
-    pub size: String,
-}
-
-#[derive(Debug)]
-pub struct Directory {
-    pub path: String,
-    pub size: String,
-    pub files: Vec<File>,
 }
