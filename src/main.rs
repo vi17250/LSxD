3ec8bf99f8006b06a35d290cb3e94e628940dd77
vi17250 <v.aubinaud@protonmail.com>
2025-07-28 00:05:42 +0200

    feat: 🎸 recursively browse directories

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,52 @@
 use clap::Parser;
+use std::fs::read_dir;
 use std::io;
-mod types;
-use types::{Directory, File};
+use std::path::PathBuf;
 
-mod commands;
-use commands::{directories, files};
+mod types;
+use crate::types::Directory;
+
 mod output_color;
-use output_color::Colored;
-// use types::Lines;
+
+const ROOT_DIR: &str = ".";
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value_t = 1)]
-    deepth: u8,
+    #[arg(short, long, default_value_t = 0)]
+    deepth: i8,
 }
 
 fn main() -> io::Result<()> {
     let args = Args::parse();
     let deepth = args.deepth;
-    let mut directories: Vec<Directory> = directories::command(deepth);
+    let root_dir: PathBuf = PathBuf::from(ROOT_DIR);
 
-    directories.iter_mut().for_each(|dir| {
-        let files: Vec<File> = files::command(dir.path.clone());
-        dir.files = files;
-    });
-
-    display(&directories);
+    let mut current_dir = Directory {
+        path: root_dir.clone(),
+        children: get_dirs(root_dir),
+    };
+    current_dir.get_children(deepth);
+    dbg!(current_dir);
     Ok(())
 }
 
-fn display(directories: &Vec<Directory>) {
-    for directory in directories {
-        println!("{} {}", directory.size.coloring(), directory.path);
-        for file in &directory.files {
-            println!("\t{} {}", file.size.coloring(), file.name);
+pub fn get_dirs(root_dir: PathBuf) -> Vec<Directory> {
+    let mut dirs: Vec<Directory> = vec![];
+    if let Ok(entries) = read_dir(root_dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(file_type) = entry.file_type() {
+                    if file_type.is_dir() {
+                        let path = entry.path();
+                        let children: Vec<Directory> = Vec::new();
+                        dirs.push(Directory { path, children });
+                    }
+                } else {
+                    println!("Couldn't get file type for {:?}", entry.path());
+                }
+            }
         }
     }
+    dirs
 }
