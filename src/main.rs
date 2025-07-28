3d8eba7abb0994b8ce74e176e757ba7c12bd8721
vi17250 <v.aubinaud@protonmail.com>
2025-07-28 12:00:08 +0200

    feat: 🎸 Create new() method for Directory

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,9 @@ use std::path::PathBuf;
 mod types;
 use crate::types::Directory;
 
+mod commands;
+use crate::commands::get_dirs;
+
 mod output_color;
 
 const ROOT_DIR: &str = ".";
@@ -14,7 +17,7 @@ const ROOT_DIR: &str = ".";
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long, default_value_t = 0)]
-    deepth: i8,
+    deepth: u8,
 }
 
 fn main() -> io::Result<()> {
@@ -22,31 +25,9 @@ fn main() -> io::Result<()> {
     let deepth = args.deepth;
     let root_dir: PathBuf = PathBuf::from(ROOT_DIR);
 
-    let mut current_dir = Directory {
-        path: root_dir.clone(),
-        children: get_dirs(root_dir),
-    };
+    let mut current_dir: Directory = Directory::new(root_dir);
     current_dir.get_children(deepth);
-    dbg!(current_dir);
-    Ok(())
-}
 
-pub fn get_dirs(root_dir: PathBuf) -> Vec<Directory> {
-    let mut dirs: Vec<Directory> = vec![];
-    if let Ok(entries) = read_dir(root_dir) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                if let Ok(file_type) = entry.file_type() {
-                    if file_type.is_dir() {
-                        let path = entry.path();
-                        let children: Vec<Directory> = Vec::new();
-                        dirs.push(Directory { path, children });
-                    }
-                } else {
-                    println!("Couldn't get file type for {:?}", entry.path());
-                }
-            }
-        }
-    }
-    dirs
+    dbg!(&current_dir);
+    Ok(())
 }
